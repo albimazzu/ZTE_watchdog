844a9991d8ea980cc8974a56fc934b51eabe86b6
A.Mazzucchelli <albymazzu@yahoo.it>
2025-04-01 20:29:01 +0200

    add first_start_timeout, ping_interval

--- zte_watchdog.py
+++ zte_watchdog.py
@@ -21,12 +21,13 @@ def reboot_router(openwrt_ip, openwrt_user, openwrt_password):
     print("Connectivity lost. Rebooting OpenWrt router...")
     os.system(f"sshpass -p '{openwrt_password}' ssh -o StrictHostKeyChecking=no {openwrt_user}@{openwrt_ip} 'reboot'")
 
-def monitor_internet(zte_ip, zte_password, openwrt_ip, openwrt_user, openwrt_password):
+def monitor_internet(zte_ip, zte_password, openwrt_ip, openwrt_user, openwrt_password, first_start_timeout, ping_interval):
     """Monitors internet connectivity and handles reboot if necessary."""
     global first_start
 
     while True:        
 
+        first_start_timer = 0
         while first_start:
             if is_internet_reachable():
                 print("Internet is reachable, first_start = False")    
@@ -34,7 +35,13 @@ def monitor_internet(zte_ip, zte_password, openwrt_ip, openwrt_user, openwrt_pas
             else:
                 print("First start ping failed.")
 
-            time.sleep(20)
+            time.sleep(10)
+            first_start_timer = first_start_timer + 10
+            print(f"first_start_timer:{first_start_timer}, first_start_timeout:{first_start_timeout}")
+            if first_start_timer > first_start_timeout:
+                print(f"first_start_timeout elapsed, internet not available. Continue execution.")
+                first_start = False
+
 
             
         if is_internet_reachable():
@@ -55,7 +62,7 @@ def monitor_internet(zte_ip, zte_password, openwrt_ip, openwrt_user, openwrt_pas
                 reboot_router(openwrt_ip, openwrt_user, openwrt_password)
                 print("Reboot executed")
                 first_start = True    
-        time.sleep(20)  # Wait 10 seconds before the next ping
+        time.sleep(ping_interval)  # Wait ping_interval seconds before the next ping
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Monitor internet connectivity and reboot routers if needed.")
@@ -64,8 +71,10 @@ if __name__ == "__main__":
     parser.add_argument("--openwrt_ip", type=str, required=True, help="IP address of the OpenWrt router.")
     parser.add_argument("--openwrt_user", type=str, required=True, help="Username for the OpenWrt router.")
     parser.add_argument("--openwrt_password", type=str, required=True, help="Password for the OpenWrt router.")
+    parser.add_argument("--first_start_timeout", type=int, default=120, required=False, help="Wait for internet connection before start regular check")
+    parser.add_argument("--ping_interval", type=int, default=10, required=False, help="Ping interval time seconds")
 
     args = parser.parse_args()
 
-    monitor_internet(args.zte_ip, args.zte_password, args.openwrt_ip, args.openwrt_user, args.openwrt_password)
+    monitor_internet(args.zte_ip, args.zte_password, args.openwrt_ip, args.openwrt_user, args.openwrt_password, args.first_start_timeout, args.ping_interval)
    
